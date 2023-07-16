b86f45a03ae7c4c34c149429c874c416b0f3ed7f
Lina <117147821+LinA636@users.noreply.github.com>
2023-07-16 23:00:37 +1200

    write #setup_board

--- lib/chessBoard.rb
+++ lib/chessBoard.rb
@@ -1,14 +1,47 @@
 require "matrix"
 
 class ChessBoard
+  attr_accessor :board
   def initialize
-    @grid = Matrix.build(8,8){Field.new}
+    @board = Matrix.build(8,8).each_with_index {|row, col| Field.new([row, col])}
     setup_board
   end
 
+
+
   def setup_board
-    # Initialize the board with pieces
-    # ...
+    self.board.row(0).each_with_index{|field, column| field.piece = Pawn.new(:black,[0,column])}
+    self.board.row(1).each_with_index do |field, column| 
+      case column
+      when 0 || 7
+        field.piece = Rook.new(:black, [1,column])
+      when 1 || 6
+        field.piece = Knight.new(:black, [1,column])
+      when 2 || 5
+        field.piece = Bishop.new(:black, [1,column]),
+      when 3
+        field.piece = Queen.new(:black, [1,column])
+      when 4
+        field.piece = King.new(:black, [1,column])
+      end
+    end
+
+    self.board.row(6).each_with_index do |field, column| 
+      case column
+      when 0 || 7
+        field.piece = Rook.new(:black, [1,column])
+      when 1 || 6
+        field.piece = Knight.new(:black, [1,column])
+      when 2 || 5
+        field.piece = Bishop.new(:black, [1,column]),
+      when 3
+        field.piece = Queen.new(:black, [1,column])
+      when 4
+        field.piece = King.new(:black, [1,column])
+      end
+    end
+    
+    self.board.row(7).each_with_index{|field, column| field.piece = Pawn.new(:white,[7,column])}
   end
 
   def move(from, to)
@@ -23,9 +56,9 @@ class ChessBoard
 
   def print_board
     puts "  a b c d e f g h"
-    @grid.each_with_index do |row, i|
+    @board.each_with_index do |row, i|
       print "#{8 - i} "
-      row.each { |piece| print "#{piece} " }
+      row.each { |field| print "#{field} " }
       puts
     end
   end
