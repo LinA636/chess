75478451ce0576ca1a7e58d9f631f4c5ba744387
Lina <117147821+LinA636@users.noreply.github.com>
2023-07-16 22:02:17 +1200

    write TDD for class queen

---
+++ lib/queen.rb
@@ -0,0 +1,20 @@
+require_relative "../lib/piece"
+
+class Queen < Piece
+
+  MOVEMENTS =  Array.new(8) {|i| [[0,i], [i,0], [-i, 0], [0, -i], [i, i], [-i, -i], [-i, i], [i, -i]] unless i.zero?}.compact.flatten(1).freeze
+
+  def initialize(color, position)
+    if color == :white
+      symbol = "\u{2655}"
+    elsif color == :black
+      symbol = "\u{265B}"
+    end
+    super(color, symbol, position)
+  end
+
+  def next_movements
+    super(MOVEMENTS)
+  end
+
+end
---
+++ spec/queen_spec.rb
@@ -0,0 +1,49 @@
+require_relative "../lib/queen"
+
+describe Queen do
+  describe '#initialize' do
+    context 'when color is white' do
+      subject(:piece){described_class.new(:white,[1,2])}
+
+      it 'sets the color, symbol and position' do
+        expect(piece.color).to eq(:white)
+        expect(piece.symbol).to eq("\u{2655}")
+        expect(piece.position).to eq([1,2])
+        expect(piece.captured).to be false
+      end
+    end
+
+    context 'when color is black' do
+      subject(:piece){described_class.new(:black,[0,0])}
+
+      it 'sets the color, symbol and position' do
+        expect(piece.color).to eq(:black)
+        expect(piece.symbol).to eq("\u{265B}")
+        expect(piece.position).to eq([0,0])
+        expect(piece.captured).to be false
+      end
+    end
+  end
+
+  describe '#next_movements' do
+    context 'when piece is in the left top corner' do
+      subject(:piece_moving){described_class.new(:white,[0,0])}
+
+      it 'returns valid next movements' do
+        expected_movements = [[1,0],[2,0],[3,0],[4,0],[5,0],[6,0],[7,0], [0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],
+                                [1,1],[2,2],[3,3],[4,4],[5,5],[6,6],[7,7]]
+        expect(piece_moving.next_movements).to match_array(expected_movements)
+      end
+    end
+
+    context 'when piece in the middle of the board' do
+      subject(:piece_moving){described_class.new(:black,[3,3])}
+
+      it 'returns valid next movements' do
+        expected_movements = [[3,0],[3,1],[3,2],[3,4],[3,5],[3,6],[3,7],[0,3],[1,3],[2,3],[4,3],[5,3],[6,3],[7,3],
+                              [0,0],[1,1],[2,2],[4,4],[5,5],[6,6],[7,7], [6,0],[5,1],[4,2],[2,4],[1,5],[0,6]]
+        expect(piece_moving.next_movements).to match_array(expected_movements)
+      end
+    end
+  end
+end
