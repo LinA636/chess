6c8a063a38d29bcfad1ae1fdf5951c943464ba34
Lina <117147821+LinA636@users.noreply.github.com>
2023-07-16 18:04:24 +1200

    add condition into next_movement in parent-class piece to distinguish between multiple and single movements

--- lib/pawn.rb
+++ lib/pawn.rb
@@ -3,7 +3,7 @@ class Pawn < Piece
   attr_accessor :first_move_done
 
   MOVEMENTS_WHITE = [[-1, 0], [-2,0]].freeze
-  MOVEMENTS_Black = [[1,0], [2,0]].freeze
+  MOVEMENTS_BLACK = [[1,0], [2,0]].freeze
   TAKING_MOVEMENTS_WHITE = [[-1,1], [-1,-1]].freeze
   TAKING_MOVEMENTS_BLACK = [[1,-1], [1,1]].freeze
 
@@ -18,13 +18,10 @@ class Pawn < Piece
   end
 
   def next_movements
-    movements = (color == :white) ? MOVEMENTS_WHITE : MOVEMENTS_Black
     unless first_move_done
-      movements.map{|arr| [self.position[0]+arr[0], self.position[1]+arr[1]]}
-             .keep_if{|arr| position_on_board?(arr)} 
+      color == :white ? super(MOVEMENTS_WHITE) : super(MOVEMENTS_BLACK)
     else
-      [self.position[0]+movements[0][0], self.position[1]+movements[0][1]]
-              .keep_if{|arr| position_on_board?(arr)} 
+      color == :white ? super(MOVEMENTS_WHITE.first) : super(MOVEMENTS_BLACK.first)
     end
   end
 
--- lib/piece.rb
+++ lib/piece.rb
@@ -15,4 +15,13 @@ class Piece
   def position_on_board?(arr)
     arr[0].between?(0,7) && arr[1].between?(0,7)
   end
+
+  def next_movements(movements)
+    if movements[0].instance_of?(Array)
+      movements.map{|arr| [self.position[0]+arr[0], self.position[1]+arr[1]]}
+             .keep_if{|arr| position_on_board?(arr)} 
+    else
+      [self.position[0]+movements[0], self.position[1]+movements[1]].keep_if{|arr| position_on_board?(arr)}
+    end
+  end
 end
